b0f2b04b17fa0b0350f136937e0b146d15fbf3ef
kawasakisora2345 <kawasakisora2345@github.com>
2025-06-07 08:07:43 +0700

    up

--- d-l-s-i-t-e.meta.js
+++ d-l-s-i-t-e.meta.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         d-l-s-i-t-e
 // @namespace    d-l-s-i-t-e.user.js
-// @version      2025.04.21
+// @version      2025.06.07
 // @description  down d-l-s-i-t-e
 // @author       You
 // @include      /^https:\/\/pl[a@]y\.d[l1]sit[e3]\.c[o0]m.+/
 // @include      /^https:\/\/pl[a@]y\.d[l1]sit[e3]\.c[o0]m\/csr\/vi[e3]wer/
+// @include      /^https:\/\/pl[a@]y\.c[o0]mip[o0]\.[a@]pp.+/
+// @include      /^https:\/\/pl[a@]y\.c[o0]mip[o0]\.[a@]pp\/csr\/vi[e3]wer/
 // @icon         https://raw.githubusercontent.com/kawashimayuma7890/dam_chau_sa/main/d-l-s-i-t-e.png
 // @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/4.2.7/fxparser.min.js
